ba9e3d4008128e3741be3ee4774e8471ecb3a47e
Carlos Rodriguez <32100019+Cvisual@users.noreply.github.com>
2023-09-12 11:47:18 -0500

    add navigation
    add react navigation and navigation stack

--- android/app/src/main/java/com/examples/MainActivity.java
+++ android/app/src/main/java/com/examples/MainActivity.java
@@ -1,5 +1,7 @@
 package com.examples;
 
+import android.os.Bundle; //for react navigation
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -14,6 +16,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "examples";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
